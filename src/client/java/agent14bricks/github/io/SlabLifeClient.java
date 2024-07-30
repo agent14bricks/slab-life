eef7604fe75ef1e6e090817064072134c2a15ec7
BuildTools <unconfigured@null.spigotmc.org>
2024-07-30 01:06:12 -0500

    initial commit

---
+++ src/client/java/agent14bricks/github/io/SlabLifeClient.java
@@ -0,0 +1,10 @@
+package agent14bricks.github.io;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlabLifeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
